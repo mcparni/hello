863669c3ee7255291b150671f754df929c39d517
Miika Pärni <miihkal@gmail.com>
2016-04-03 23:34:38 +0300

    -- Working Ship Class and basic but working board Class. Players Ships can now be built and placed to board.

--- src/sinkit/Board.java
+++ src/sinkit/Board.java
@@ -1,16 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sinkit;
 
-/**
- *
- * @author mcparni
- */
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
 public class Board {
+    
+    private final JPanel board;
+    private final int SQUARE = 50;
+    private final Color borderGrey = new Color(242, 242, 242);
+    private GridBagConstraints c;
+
     public Board() {
-        System.out.println("Board init");
+        System.out.println("Board initialized");
+        this.board = new JPanel();
+        this.board.setBackground(Color.WHITE);
+        this.board.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+              
+        int squareSize = SQUARE;
+        
+        Dimension d = new Dimension();
+        d.width = SQUARE;
+        d.height = SQUARE;
+        
+        for(int i = 1; i < 14; i++) {
+            for(int j = 1; j < 12; j++) {
+                JLabel square = new JLabel("");
+                square.setOpaque(false);
+                square.setSize(SQUARE, SQUARE);
+                square.setBackground(new Color(255, 164, 233));
+                square.setMinimumSize(d);
+                square.setPreferredSize(d);
+                square.setBorder(BorderFactory.createLineBorder(this.borderGrey));
+                c.gridx = i;
+                c.gridy = j;
+                this.board.add(square, c);
+            }
+        }
     }
+    public void addShip(int type, int x, int y) {
+        // No checks yet whether the x and y values are valid
+        
+        Ship t = new Ship();
+        t.buildShip(type);
+                
+        c.gridwidth = t.getShipWidth();
+        c.gridheight = t.getShipHeight();
+    
+        c.gridx = x;
+        c.gridy = y;
+        
+        this.board.add(t.getShip(), c, 0);
+    }
+    
+    
+    public JPanel getBoard() {
+        return this.board;
+    }
+    
+  
+      
 }
--- src/sinkit/Ship.java
+++ src/sinkit/Ship.java
@@ -1,34 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sinkit;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-/**
- *
- * @author mcparni
- */
+
 public class Ship {
     
     private final int STEP = 50;
     private int cols = 1;
     private int rows = 1;
-    
+    private int shipType = 1;
+    private JPanel ship;
     // squareOne green;
     private Color color = new Color(176, 255, 164);
 
     
     public Ship() {
-    
+        this.color = new Color(176, 255, 164);
+        this.cols = 1;   
+        this.rows = 1;
+        this.shipType = 1;
+        ship = new JPanel();
+        
     }
     
-    public JPanel Ship(int type) {
+    public int getColumns() {
+        return this.cols;
+    }
+    
+    public int getRows() {
+        return this.rows;
+    }
+    
+    public Color getColor() {
+        return this.color;
+    }
+    
+    public int getShipType() {
+        return this.shipType;
+    }
+    
+    public int getSquareSize() {
+        return this.STEP;
+    }
+    
+    public int getShipWidth() {
+        return this.cols;
+    }
+    
+    public int getShipHeight() {
+        return this.rows;
+    }
+    
+    public JPanel getShip() {
+        return this.ship;
+    }
+    
+    /**
+     @param type = valid values(1,2,4,6,8) defaults to 1
+     * 
+     * 
+     */
+    public void buildShip(int type) {
         
         switch(type) {
             case 1 :
@@ -36,57 +74,85 @@ public class Ship {
                this.color = new Color(176, 255, 164);
                this.cols = 1;
                this.rows = 1;
+               this.shipType = type;
                break;
             case 2 :
                // squareTwo blue
                this.color = new Color(164, 205, 255);
                this.cols = 2;
                this.rows = 1;
+               this.shipType = type;
                break;
             case 4 :
                // squareFour orange
                this.color = new Color(255, 206, 164);
                this.cols = 2;
                this.rows = 2;
+               this.shipType = type;
                break;
             case 6 :
                // squareSix pink 
                this.color = new Color(255, 164, 233);
                this.cols = 2;
-               this.rows = 3; 
+               this.rows = 3;
+               this.shipType = type;
                break;
             case 8 :
                // squareEight red
                this.color = new Color(255, 164, 164);
                this.cols = 2;
                this.rows = 4;
+               this.shipType = type;
                break;
             default :
                 // squareOne green;
                this.color = new Color(176, 255, 164);
                this.cols = 1;
                this.rows = 1;
+               this.shipType = 1;
                break;
                
         }
         
-        JPanel panel = new JPanel();
-        panel.setSize(this.cols * STEP, this.rows * STEP);
-        panel.setLayout(null);
+        int w = this.cols * STEP;
+        int h = this.rows * STEP;
+        Dimension d = new Dimension(w, h);
+        this.ship.setSize(w, h);
+        this.ship.setMinimumSize(d);
+        this.ship.setPreferredSize(d);
+        
+        this.ship.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = this.cols;
+        c.gridheight =  this.rows;
        
-        for(int i = 0; i < cols; i++) {
-            for(int j = 0; j < rows; j++) {
-                panel.add(buildShip((i * STEP), j* STEP));
+        for(int i = 1; i < (this.cols + 1); i++) {
+            for(int j = 1; j < (this.rows + 1); j++) {
+                c.gridx = i;
+                c.gridy = j;
+                c.gridwidth = 1;
+                c.gridheight =  1;
+                c.fill = GridBagConstraints.NONE;
+                c.weightx = 0;
+                this.ship.add(addShipBlock(), c, 0);
+                
             }
         }
-        
-        return panel;
     }
-    private JLabel buildShip(int x, int y) {
-        JLabel square = new JLabel();
+    private JLabel addShipBlock() {
+        
+        JLabel square = new JLabel("");
         square.setOpaque(true);
         square.setBackground(this.color);
-        square.setBounds(x,y,STEP,STEP);
+        Dimension d = new Dimension(this.STEP,this.STEP);
+        square.setPreferredSize(d);
+        square.setMinimumSize(d);
+ 
         return square;
     } 
     
